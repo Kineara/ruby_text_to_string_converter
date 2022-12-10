86e992c4fb544e0383a0bc063070e70f113b3235
Riley Hoock <laurenh8916@protonmail.com>
2022-12-09 17:19:35 -0700

    added quote detection and eof comma deletion

--- ruby-string-converter.rb
+++ ruby-string-converter.rb
@@ -1,7 +1,25 @@
-input_array = File.readlines("input.txt")
-output_file = File.open("./output.txt", "w")
+input_array = File.readlines('input.txt')
+output_file = File.open('./output.txt', 'w')
+
+output_array = []
 
 input_array.each do |element|
   # Remove the indexes from element if input has no newline chars
-  output_file.write('"' + element[0..-2] + '",')
+
+  words_array = element.split(' ')
+  output = ''
+  words_array.each do |word|
+    if word[0] && word[-1] == '"'
+      '\\' + word + '\\'
+    elsif word[-1] == '\\n'
+      word[0..-2]
+    else
+      word
+    end
+    output = '"' + words_array.join(' ') + '"'
+  end
+
+  output_array.push(output)
 end
+
+output_file.write(output_array.join(',')[0..-1])
